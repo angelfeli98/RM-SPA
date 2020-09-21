6a811c1e70b66531f3f66be6ab25223b33f9b259
Felipe Angel Regalado <36333913+angelfeli98@users.noreply.github.com>
2020-09-21 05:14:14 -0500

    Update af2b8e63623e456b9ae0.main.js

--- docs/af2b8e63623e456b9ae0.main.js
+++ docs/af2b8e63623e456b9ae0.main.js
@@ -1,6 +1,6 @@
 (function(a){function b(d){if(c[d])return c[d].exports;var e=c[d]={i:d,l:!1,exports:{}};return a[d].call(e.exports,e,e.exports,b),e.l=!0,e.exports}var c={};return b.m=a,b.c=c,b.d=function(a,c,d){b.o(a,c)||Object.defineProperty(a,c,{enumerable:!0,get:d})},b.r=function(a){'undefined'!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(a,Symbol.toStringTag,{value:'Module'}),Object.defineProperty(a,'__esModule',{value:!0})},b.t=function(a,c){if(1&c&&(a=b(a)),8&c)return a;if(4&c&&'object'==typeof a&&a&&a.__esModule)return a;var d=Object.create(null);if(b.r(d),Object.defineProperty(d,'default',{enumerable:!0,value:a}),2&c&&'string'!=typeof a)for(var e in a)b.d(d,e,function(b){return a[b]}.bind(null,e));return d},b.n=function(a){var c=a&&a.__esModule?function(){return a['default']}:function(){return a};return b.d(c,'a',c),c},b.o=function(a,b){return Object.prototype.hasOwnProperty.call(a,b)},b.p='',b(b.s=2)})([function(a,b){'use strict';Object.defineProperty(b,'__esModule',{value:!0}),b.characterComponent=void 0,b.characterComponent=(a)=>`
     <div class="card cardCustom">
-        <a href="http://localhost:8080/#/character/${a.id}">
+        <a href="https://angelfeli98.github.io/RM-SPA/#/character/${a.id}">
             <img src="${a.image}" class="card-img-top" alt="${a.name}" loading="lazy">
         </a>
         <h4 class="card-title">${a.name}</h4>
@@ -38,7 +38,7 @@
             ${'unknown'==b.name?`
                     ${b.name}
             `:`
-                <a href="${`http://localhost:8080/#/charactersAt/${'Location'==a?e:g}`}">
+                <a href="${`https://angelfeli98.github.io/RM-SPA/#/charactersAt/${'Location'==a?e:g}`}">
                     ${b.name}
                 </a>
                 `}
@@ -58,7 +58,7 @@
             </div>
         </div>
     `}},function(a,b,c){'use strict';Object.defineProperty(b,'__esModule',{value:!0}),b.LocationsTemplate=void 0;const d=c(1);b.LocationsTemplate=async()=>{const a=(await(await fetch('https://rickandmortyapi.com/api/location/')).json()).results,b=a.reduce((a,b)=>`
-        <a href="http://localhost:8080/#/charactersAt/${b.id}" class="list-group-item list-group-item-action list-group-item-dark location-list">
+        <a href="https://angelfeli98.github.io/RM-SPA/#/charactersAt/${b.id}" class="list-group-item list-group-item-action list-group-item-dark location-list">
             ${b.name}
         </a>
     `+a,''),c=new d.DataFunction;return c.locations=a,document.title='RM | Locations',`
